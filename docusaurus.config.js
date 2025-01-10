d28071e1fb4c3cb40d9c37cc625364cdbbe8ccef
erasernoob <1490910338@qq.com>
2025-01-10 21:20:20 +0800

    update: update the logo & fix some configuration stuff

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
   organizationName: 'bigdata-io', // Usually your GitHub org/user name.
   projectName: 'doc', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
