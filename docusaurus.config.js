04bef587165a32bd62e5bdc69fbfe97b6d668d94
erasernoob <1490910338@qq.com>
2025-01-10 22:53:31 +0800

    update: add the custom layout file & change some of the style for the homepage

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/bigdata-io/docs/issues',
+            'https://github.com/bigdata-io/docs/',
         },
         blog: {
           showReadingTime: true,
@@ -57,7 +57,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/bigdata-io/docs/issues',
+            'https://github.com/bigdata-io/docs/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-              
+              Start From Here
           </Link>
         </div>
       </div>
